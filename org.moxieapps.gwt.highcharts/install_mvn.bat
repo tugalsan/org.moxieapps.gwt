bbae260057e80bf2b3a566d50132f7eb94c65fed
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-11-27 10:59:30 +0300

    Initial commit

---
+++ org.moxieapps.gwt.highcharts/install_mvn.bat
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=org.moxieapps.gwt.highcharts-1.7.0.jar -DgroupId=org.moxieapps.gwt -DartifactId=highcharts -Dversion=1.7.0 -Dpackaging=jar
---
+++ org.moxieapps.gwt.uploader/install_mvn.bat
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=org.moxieapps.gwt.uploader-1.1.0.jar -DgroupId=org.moxieapps.gwt -DartifactId=uploader -Dversion=1.1.0 -Dpackaging=jar
